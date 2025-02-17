99588e396d6380d0f38a6ad1f19cd492af6073bd
flexthink <flexthink@users.noreply.github.com>
2025-02-17 14:55:31 -0500

    DASB: VALL-E: Add LM head freezing

--- benchmarks/DASB/LJSpeech/TTS/valle/train.py
+++ benchmarks/DASB/LJSpeech/TTS/valle/train.py
@@ -258,6 +258,12 @@ class VALLEBrain(sb.Brain):
         only the non-autoregressive part"""
         epoch = self.hparams.epoch_counter.current
         self.train_ar, self.train_nar = True, True
+        lm_head = (
+            self.modules.model.module.lm_head
+            if hasattr(self.modules.model, "module")
+            else self.modules.model.lm_head
+        )
+        lm_head.requires_grad_(True)
         if self.hparams.audio_tokens_per_step == 1:
             # NOTE: If there is only one track it's autoregressive
             self.train_nar = False
@@ -268,6 +274,8 @@ class VALLEBrain(sb.Brain):
             and epoch <= (self.hparams.number_of_epochs_ar + self.hparams.number_of_epochs_nar)
         ):
             self.train_ar = False
+            if self.hparams.freeze_lm_head:
+                lm_head.requires_grad_(False)
 
     def is_eval_epoch(self, epoch):
         """Determines whether or not evaluation should be performed
